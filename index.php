8e66b084baf093b629aae6859b73f390ccfce6af
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-05-15 16:36:36 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$router = new AltoRouter();
+
+$router->setBasePath('/Cinetech');
+
+
+// map homepage
+$router->map( 'GET', '/', function() {
+    echo"to=choupie cest 
+    le best";
+});
+
+// map user details page
+$router->map( 'GET', '/user', function(  ) {
+});
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+?>
